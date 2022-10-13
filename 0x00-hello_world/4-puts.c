e6acee517a33b92f7d24653fee1382eb2ceb36a6
abrehamdesalegn <meetabdesu@gmail.com>
2022-10-13 12:56:45 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 /**
- * puts function
+ *  * main - Entry point
+ *   *
+ *    * Description: 'the program's description'
+ *     *
+ *      * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char test[] = "\"Programming is like building a multilingual puzzle";
-	
+
 	puts(test);
 	return (0);
 }
