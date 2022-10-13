3ea5931a5af7a3de985bd7e34e4b287d4e598f4e
abrehamdesalegn <meetabdesu@gmail.com>
2022-10-13 12:23:43 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
+/**
+ * puts function
+ */
 int main(void)
 {
-	/**
-	 * puts function
-	 */
-	
 	char test[] = "\"Programming is like building a multilingual puzzle";
 	
 	puts(test);
-	
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
+/**
+ * printf
+ */
 int main(void)
-{
-	/**
-	 * printf function
-	 */
-	
+{	
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
 	return (0);
 }
