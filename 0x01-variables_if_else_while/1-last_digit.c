904d66c47e8ac0fc5bfc2b67e2917e7babd595c6
root <root@DESKTOP-KACNSM0.localdomain>
2022-10-14 12:42:49 -0700

    lower

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -29,7 +29,7 @@ int main(void)
 	else if (n < 6 && n != 0)
 	{
 		printf("Last digit of %d is ", n);
-		printf("%d and is less than 6 not 0\n", last);
+		printf("%d and is less than 6 and not 0\n", last);
 	}
 	return (0);
 }
