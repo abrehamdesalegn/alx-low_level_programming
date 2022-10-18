251fb423c5f849b525c2fd6dd09e5a9213ed103e
abrehamdesalegn <meetabdesu@gmail.com>
2022-10-18 14:07:38 +0300

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * a - input
+ * b - input
+ * Return: Always 0 (Success)
+ * @a: first member
+ * @b: input
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
