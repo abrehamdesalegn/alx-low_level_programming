c3f06f7fd1fa9d9131e29a1062359de514cb2aac
abrehamdesalegn <meetabdesu@gmail.com>
2022-10-24 17:45:23 +0300

    srtlen

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,15 @@
 #include "main.h"
 /**
- * __puts - check the code
+ * _puts - check the code
  *
  * Return: Always 0.
  * @str: para1
  */
 void _puts(char *str)
 {
-	_putchar(str);
+	while (*str != '\0')
+	{
+	_putschar(str);
+	*str++;
+	}
 }
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 void _putchar(char *str)
 {
-	puts(str);
+	putchar(*str);
 }
