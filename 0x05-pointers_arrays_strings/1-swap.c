70d960219a7c08b92ec2a89495eeec37f43b4ac3
abrehamdesalegn <meetabdesu@gmail.com>
2022-10-24 13:47:08 +0300

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ *
+ * Return: Always 0.
+ * @a: para1
+ * @b: para2
+ */
+void swap_int(int *a, int *b)
+{
+	int sw;
+	sw = *a;
+	*a = *b;
+	*b = sw;
+ }
