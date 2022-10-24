12c56838a13eec26061a2696028c56bab138e470
abrehamdesalegn <meetabdesu@gmail.com>
2022-10-24 17:10:52 +0300

    srtlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - check the code
+ *
+ * Return: Always 0.
+ * @s: ara1
+ */
+int _strlen(char *s)
+{
+	unsigned int count = 0;
+
+	while(*s!='\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
