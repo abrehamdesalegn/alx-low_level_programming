684cc0b9eba7fe2b56e7c8febc80de7920505e2b
root <root@DESKTOP-KACNSM0.localdomain>
2022-10-26 10:54:57 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcat - check the code
+ *
+ * Return: Always 0.
+ *@*dest: para1
+ *@*src: para2
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	return(dest);
+}
