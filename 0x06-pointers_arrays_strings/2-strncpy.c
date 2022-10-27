7cc08476505d9982adad156ed6745b88488a4729
root <root@DESKTOP-KACNSM0.localdomain>
2022-10-26 22:29:31 -0700

    cmp

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-        int j;
+	int j;
 
-        for (j = 0; j < n && src[j] != '\0'; j++)
-        {
-                dest[j] = src[j];
-        }
-        for ( ; j < n ; j++)
-        {
-                dest[j] = '\0';
-        }
-        return (dest);
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	for ( ; j < n ; j++)
+	{
+		dest[j] = '\0';
+	}
+	return (dest);
 }
