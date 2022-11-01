db51c9a6f94f8ee74a4a89fd381a5e1c4f00f41d
root <root@DESKTOP-KACNSM0.localdomain>
2022-11-01 06:15:31 -0700

    strbrk

---
+++ 0x07-pointers_arrays_strings/4-strbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - check the code
+ *
+ * Return: Always 0.
+ *@s: para1
+ *@accept: para
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (accept[i] == *s)
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (s);
+}
