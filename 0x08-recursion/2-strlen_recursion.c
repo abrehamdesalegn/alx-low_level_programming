0513951ccc824034873a88283a45a082cc71401b
abrehamdesalegn <meetabdesu@gmail.com>
2022-11-02 18:16:31 +0300

    factorial

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -14,8 +14,8 @@ int _strlen_recursion(char *s)
 	if (*s != '\0')
 	{
 		s++;
-		count=_strlen_recursion(s);
+		count = _strlen_recursion(s);
 		count++;
 	}
-	return (count);	
+	return (count);
 }
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - check the code
+ *
+ * Return: Always 0.
+ *@n: para
+ */
+int factorial(int n)
+{
+	if (n > 1)
+	{
+		n *= factorial(n - 1);
+		return (n);
+	}
+	else if (n == 0 || n == 1)
+		return (1);
+	else
+		return (-1);	
+}
