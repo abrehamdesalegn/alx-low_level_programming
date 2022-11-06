cd8efa5198352440ce3cb2e6b2ecbb569f126a75
root <root@DESKTOP-KACNSM0.localdomain>
2022-11-06 00:36:58 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - func
+*@argc: para
+*@argv: para
+*Return: 0
+*/
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int c;
+
+	c = 0;
+	while (argc >= 0)
+	{
+		c++;
+		argc--;
+	}
+	printf("%d\n", c - 2);	
+	return (0);
+}
