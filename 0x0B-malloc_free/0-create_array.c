676e4da829993302dc22b54b810684ab8065d387
abrehamdesalegn <meetabdesu@gmail.com>
2022-11-07 16:39:57 +0300

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ar = (char*) malloc(sizeof(char) * size);
+	ar = (char *) malloc(sizeof(char) * size);
 	if (ar == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
