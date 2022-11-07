201f458fd6be7a033d69572c2a550e00eb2d4f6c
abrehamdesalegn <meetabdesu@gmail.com>
2022-11-07 16:54:10 +0300

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - prints buffer in hexa
+ * @str: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_strdup(char *str)
+{
+	char *ar = str;
+
+	return (ar);
+}
