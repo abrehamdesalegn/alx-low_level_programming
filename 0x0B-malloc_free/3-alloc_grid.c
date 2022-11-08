c989cd6e19a27f3a249531c442baad1afc4b24aa
abrehamdesalegn <meetabdesu@gmail.com>
2022-11-08 12:00:59 +0300

    twodime

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - prints buffer in hexa
+ * @width: the size of the memory to print
+ *@height: string
+ * Return: Nothing.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ar;
+	int i, j;
+
+	if (width == 0 && height == 0)
+		return (NULL);
+	ar = (int **) malloc(sizeof(int *) * height);
+	if (ar == NULL)
+		return (NULL);
+	for (j = 0; j < width; j++)
+		{
+			ar[j] = (int *) malloc(sizeof(int) * width);
+		}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ar[i][j] = 0;
+		}
+	}
+	return (ar);
+}
