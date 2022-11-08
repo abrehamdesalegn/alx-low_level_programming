77201bc15f7ee66612e8b86f2e3f4303e968b638
abrehamdesalegn <meetabdesu@gmail.com>
2022-11-08 07:54:23 +0300

    dup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,17 +1,23 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
- * _strdup - check the code for ALX School students.
+ * create_array - prints buffer in hexa
+ * @str: the size of the memory to print
  *
- * Return: Always 0.
- * @str: para
+ * Return: Nothing.
  */
 char *_strdup(char *str)
 {
 	char *ar;
-
-	ar = (char *)malloc(sizeof(char));
-	ar = str;
+	int i;
+	
+	i = 0;
+	ar = (char *) malloc(sizeof(str));
+	while (i < sizeof(str))
+	{
+		ar[i] = str[i];
+		i++;
+	}
 	return (ar);
 }
