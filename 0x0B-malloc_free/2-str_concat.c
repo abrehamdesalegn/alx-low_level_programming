10de4659f464d21a641f27fa79a79cbdb1325f8a
AbrehamDesalegn <114426096+abrehamdesalegn@users.noreply.github.com>
2022-11-08 09:44:30 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,11 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	len1 = 0;
 	len2 = 0;
-	if (s1 == '\0')
+	if (s1 == NULL)
 		return (s2);
-	else if (s2 == '\0')
+	else if (s2 == NULL)
 		return (s1);
-	else if (s2 == '\0' && s1 == '\0')
+	else if (s2 == NULL && s1 == NULL)
 		return (NULL);
 	else
 	{
