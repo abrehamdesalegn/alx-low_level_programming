2c03be4160354901788291780003b56ae2259388
abrehamdesalegn <meetabdesu@gmail.com>
2022-11-08 09:29:32 +0300

    concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * str_concat - prints buffer in hexa
+ * @s1: the size of the memory to print
+ *@s2: string
+ * Return: Nothing.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ar;
+	unsigned int i, len1, len2;
+
+	i = 0;
+	len1 = 0;
+	len2 = 0;
+	if (s2 == NULL)
+		return (s1);
+	while (s1[i])
+	{
+		len1++;
+		i++;
+	}
+	len1 -= 1;
+	i = 0;
+	while (s2[i])
+	{
+		len2++;
+		i++;
+	}
+	ar = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
+	i = 0;
+	while (i <= len1 + len2)
+	{
+		if (i <= len1)
+			ar[i] = s1[i];
+		else if (i > len1)
+			ar[i] = s2[i - 1 - len1];
+		i++;
+	}
+	return (ar);
+}
