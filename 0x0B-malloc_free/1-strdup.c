d773ae01f6a717fec484ae73630e67aa6c977832
AbrehamDesalegn <114426096+abrehamdesalegn@users.noreply.github.com>
2022-11-08 08:11:59 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,13 +10,14 @@
 char *_strdup(char *str)
 {
 	char *ar;
-	unsigned int i;
+	unsigned int i, size;
 
 	i = 0;
+	size = sizeof(str) - 2;
 	if (str == NULL)
 		return (NULL);
-	ar = (char *) malloc(sizeof(char));
-	while (i < sizeof(str))
+	ar = (char *) malloc(sizeof(str));
+	while (i < size)
 	{
 		ar[i] = str[i];
 		i++;
