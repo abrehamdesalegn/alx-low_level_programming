d2a507c46bbb268f0b96f06e7b1ea31b8089dbe9
AbrehamDesalegn <114426096+abrehamdesalegn@users.noreply.github.com>
2022-11-09 09:32:56 -0800

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,10 +7,17 @@
 */
 int main(int argc, char *argv[])
 {
+	int c, i;
+
+	c = 0;
 	while (argc >= 0)
 	{
-		printf("%s\n", argv[argc]);
+		c++;
 		argc--;
+	}
+	for (i = 0; i < c; i++)
+	{
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
