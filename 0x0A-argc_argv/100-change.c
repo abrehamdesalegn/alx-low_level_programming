0972303f3c95e16fc3a3a4a234c6d4d35d277c14
root <root@DESKTOP-KACNSM0.localdomain>
2022-11-09 12:21:39 -0800

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,30 +21,25 @@ int main(int argc, char *argv[])
 	{
 		j = atoi(argv[1]);
 		k = 0;
-		for (i = j; i >= 25; )
+		for (i = j; i >= 25; i -= 25)
 		{
 			k++;
-			i -= 25;
 		}
-		for ( ; i >= 10; )
+		for ( ; i >= 10; i -= 10)
 		{
 			k++;
-			i -= 10;
 		}
-		for ( ; i >= 5; )
+		for ( ; i >= 5; i -= 5)
 		{
 			k++;
-			i -= 5;
 		}
-		for ( ; i >= 2; )
+		for ( ; i >= 2; i -= 2)
 		{
 			k++;
-			i -= 2;
 		}
-		for ( ; i >= 1; )
+		for ( ; i >= 1; i -= 1)
 		{
 			k++;
-			i -= 1;
 		}
 		printf("%d\n", k);
 	}
