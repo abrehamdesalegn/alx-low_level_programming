9b94ebce369a69f5ab7d69285a8d815a77ae36ae
AbrehamDesalegn <114426096+abrehamdesalegn@users.noreply.github.com>
2022-11-10 17:39:01 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2++;
 		i++;
 	}
-	ar = malloc(sizeof(char) * (len1 + 1 + len2 + 1));
+	ar = malloc(sizeof(char) * (len1 + 1 + j + 1));
 	if (ar == NULL)
 		return (NULL);
 	i = 0;
