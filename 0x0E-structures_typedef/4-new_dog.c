d7cc7a3e6310fa6f132ba0f748c4b3c9a7d19991
root <root@DESKTOP-KACNSM0.localdomain>
2022-11-15 11:33:32 -0800

    new

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *init_dog - func
+ *@d: struct
+ *@name: name
+ *@age: age
+ *@owner: owner
+ *Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
