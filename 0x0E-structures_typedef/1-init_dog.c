76d9f8fd6eaa4ced6e2b9180152ad008ea22d862
root <root@DESKTOP-KACNSM0.localdomain>
2022-11-15 10:43:57 -0800

    init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,7 +4,7 @@
  *@d: struct
  *@name: name
  *@age: age
- *@owner:
+ *@owner: owner
  *Return: Always 0.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -14,5 +14,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d.name = name;
 	d.age = age;
 	d.owner = owner;
-	return (d);
 }
