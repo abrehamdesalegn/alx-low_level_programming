42512716f6a9f4f702264088f9d3618682ae2fa2
AbrehamDesalegn <114426096+abrehamdesalegn@users.noreply.github.com>
2022-11-19 11:04:14 -0800

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,18 +15,18 @@
 	
  	a = atoi(argv[1]);
  	b = atoi(argv[3]);
- 	*op = *argv[2];
- 	if (argc != 3)
+ 	op = argv[2];
+ 	if (argc != 4)
  	{
- 		printf("Error");
+ 		printf("Error\n");
  		exit(98);
 	}
- 	if (argv[2] != "+" && argv[2] != "*" && argv[2] != "-" && op != "/" && op != "%")
- 	{
- 		printf("Error");
- 		exit(99);
+ 	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
 	}
-	if (op == "/" || op == "%" && b == 0)
+	if ((*op == '/' || *op == '%') && b == 0)
  	{
  		printf("Error");
  		exit(100);
