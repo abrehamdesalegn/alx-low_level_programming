5b0750041fbcfa9546e7185a0b79e46fd1a288a0
abrehamdesalegn <meetabdesu@gmail.com>
2022-11-25 18:20:02 +0300

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - check the code
+ * @h: para
+ * Return: Always 0.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node;
+
+	node = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+	
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
