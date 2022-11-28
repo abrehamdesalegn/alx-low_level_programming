fe7593f55927cd275db8812e2a7598256e119edb
AbrehamDesalegn <114426096+abrehamdesalegn@users.noreply.github.com>
2022-11-28 09:22:10 -0800

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,8 +11,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *node;
+	int i, len;
 
+	if (head == NULL)
+		return (NULL);
 	node = (list_t *)malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
+	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	node->len = len;
 	node->str = strdup(str);
 	node->next = *head;
 	*head = node;
