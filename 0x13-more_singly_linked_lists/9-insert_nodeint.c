023befce6a63d4bb48844f8fa05a0fd0793d1f9c
AbrehamDesalegn <114426096+abrehamdesalegn@users.noreply.github.com>
2022-11-30 17:31:02 +0300

    Create 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - check the code
+ * @head: para
+ * @n: para
+ * @idx: para
+ * Return: Always 0.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *prenode, *nextnode, *newnode;
+	unsigned int i;
+
+	prenode = *head;
+	newnode = (listint_t*)malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	if (idx == 0)
+	{
+		newnode->next = (*head)->next;
+		*head = newnode;
+	}
+	else
+	{
+		for (i = 1; i < idx; i++)
+		{
+			prenode = prenode->next;
+		}
+		if (prenode == NULL)
+			return (NULL);
+		nextnode = prenode->next;
+		prenode->next = newnode;
+		newnode->next = nextnode;	
+	}
+	return (newnode);
+}
