d3888be8d3ed8e0f6f9d502e015fdeb41dc0b7f7
AbrehamDesalegn <114426096+abrehamdesalegn@users.noreply.github.com>
2022-11-30 09:46:37 -0800

    Create 10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - check the code
+ * @head: para
+ * @n: para
+ * Return: Always 0.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *tmp1, *tmp2;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	if (index == 0)
+		*head = (*head)->next;
+	else
+	{
+		tmp = *head;
+		for (i = 1; i < index; i++)
+		{
+			tmp = tmp->next;
+		}
+		if (tmp == NULL)
+			return (-1);
+		tmp1 = tmp->next;
+		tmp2 = tmp1->next;
+		tmp->next = tmp2;
+		free(tmp1);
+	}
+	return (1);
+}
