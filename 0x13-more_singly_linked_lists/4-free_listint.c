e8545bd94d946e6d4cc5d189ca9d7cdc01b79b20
AbrehamDesalegn <114426096+abrehamdesalegn@users.noreply.github.com>
2022-11-30 09:45:42 +0300

    Create 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - check the code
+ * @head: para
+ *
+ * Return: Always 0.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	
+	while (head)
+	{
+		node = head;
+		free(head);
+		head = node->next;
+	}
+}
