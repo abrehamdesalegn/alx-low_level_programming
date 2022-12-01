7eb4c92bb9c32619813aee8229df1b1d0a79007d
AbrehamDesalegn <114426096+abrehamdesalegn@users.noreply.github.com>
2022-12-01 09:31:12 -0800

    Create 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+unsigned int po(int k)
+{
+	if (k == 0)
+		return (1);
+	else if (k == 1)
+		return (2);
+	else
+		return (2 * po(k - 1));
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, l, j;
+	int k;
+
+	k = 0;
+	i = 0;
+	l = 0;
+	while (b[k])
+	{
+		k++;
+	}
+	k--;
+	j = k;
+	while (k >= 0)
+	{
+		i = b[k] - 48;
+		if (i == 0)
+		{
+			k--;
+			continue;
+		}
+		else if (i == 1)
+		{
+			l += po(j - k);
+		}
+		else
+		{
+			return NULL;
+		}
+		k--;
+	}
+	return (l);
+}
