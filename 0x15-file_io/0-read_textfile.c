3021a2974674211135cda26d69ac22c166ac59cd
abrehamdesalegn <meetabdesu@gmail.com>
2022-12-05 18:20:26 +0300

    read

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * read_textfile - check the code
+ * @filename: para
+ * @letters: para
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd;
+	char *buf;
+
+	buf = malloc(sizeof(char) * letters);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+	read(fd, buf, letters);
+	printf("%s\n", buf);
+	close(fd);
+	return (letters);
+}
