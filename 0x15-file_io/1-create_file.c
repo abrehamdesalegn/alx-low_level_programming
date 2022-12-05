bcb48e6f93c0b7c23471b6372ff0352d0e11d6ca
root <root@DESKTOP-KACNSM0.localdomain>
2022-12-05 11:21:49 -0800

    creat

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * create_file - check the code
+ * @filename: para
+ * @text_content: para
+ * Return: Always 0.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, wr;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (file == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	wr = write(file, text_content, strlen(text_content));
+	if (wr == -1)
+		return (-1);
+	return (1);
+}
