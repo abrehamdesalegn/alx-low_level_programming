8fd4f061a9e10717c5110559c507dfb494227188
AbrehamDesalegn <114426096+abrehamdesalegn@users.noreply.github.com>
2022-12-05 10:05:36 -0800

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -27,12 +27,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	rd = read(fd, buf, letters);
 	buf[letters] = '\0';
-	if (rd == -1 )
+	if (rd == -1)
 	{
 		return (0);
 	}
 	wr = write(1, buf, rd);
-	if (wr == -1 )
+	if (wr == -1)
 	{
 		return (0);
 	}
