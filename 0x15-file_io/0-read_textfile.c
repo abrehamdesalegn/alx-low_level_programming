adfcd680e782d52db17f338703ba34bb568466b1
AbrehamDesalegn <114426096+abrehamdesalegn@users.noreply.github.com>
2022-12-05 10:05:11 -0800

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,17 +17,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd, rd, wr;
 	char *buf;
 
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 	buf = malloc(sizeof(char) * letters + 1);
 	if (buf == NULL)
-	{
 		return (0);
-	}
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1 || filename == NULL)
-	{
-		return (0);
-	}
 	rd = read(fd, buf, letters);
 	buf[letters] = '\0';
 	if (rd == -1 )
