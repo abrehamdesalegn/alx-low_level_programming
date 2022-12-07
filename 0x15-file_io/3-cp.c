edef9f81d007731d12716fd22e5d91811e2ab276
abrehamdesalegn <meetabdesu@gmail.com>
2022-12-07 10:30:02 +0300

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,10 +2,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * main - check the code
  * @argc: para
@@ -25,14 +23,13 @@ int main(int argc, const char *argv[])
 	}
 	srcfile = argv[1];
 	destfile = argv[2];
-
 	op = open(srcfile, O_RDONLY);
 	if (op == -1)
 	{
 		dprintf(1, "Error: Can't read from file %s\n", srcfile);
 		exit(98);
 	}
-	buf = malloc(1024 + 1);
+	buf = malloc(sizeof(char) * 1024);
 	if (buf == NULL)
 		return (0);
 	rd = read(op, buf, 1024 / 4);
